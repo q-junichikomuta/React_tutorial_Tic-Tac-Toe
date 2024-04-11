c8358ccbb822622a81467055b01957f478519cf3
junichi komuta <komuta@q-co.jp>
2024-04-11 15:29:30 +0900

    WebSocketの接続するためのカスタムフック

---
+++ src/hooks/useSocket.ts
@@ -0,0 +1,53 @@
+import { messageBoardAtom, socketAtom } from '@/app/globalStates/atoms';
+import Message from '@/app/models/message';
+import { useAtom } from 'jotai';
+import { io } from 'socket.io-client';
+
+export const useSocket = () => {
+  const oneSideNum = 3;
+  // 各グローバル状態のAtomを用意
+  const [, setMessageBoard] = useAtom(messageBoardAtom);
+  const [, setSocket] = useAtom(socketAtom);
+
+  const socketInitializer = (socket: any) => {
+    // サーバーとの接続が確立したときの処理
+    socket.on('connect', () => {
+      console.log('Connected to the server');
+    });
+
+    // サーバーとの接続が切断されたときの処理
+    socket.on('disconnect', () => {
+      console.log('Disconnected from the server');
+    });
+
+    // サーバーからメッセージを受信したときの処理
+    socket.on('message', (newMessage: Message) => {
+      // グローバル状態のメッセージ一覧を更新
+      setMessageBoard((messageBoard) => {
+        // idが重複するメッセージを削除（一応の処理）
+        const newMessageBoard = Array.from(new Map(messageBoard.map((message) => [message.id, message])).values());
+        // 新しいメッセージを追加
+        newMessageBoard.push(newMessage);
+        return newMessageBoard;
+      });
+    });
+  };
+
+  // WebSocketに接続する関数
+  const socketConnect = async () => {
+    // WebSocketサーバーに接続するにはまずHTTPサーバーに接続してWebSocketのエンドポイントを取得する必要がある
+    await fetch('http://localhost:3000/api/sockets', { method: 'POST' });
+
+    // WebSocketサーバーに接続
+    const socket = io({ autoConnect: false });
+    socket.connect();
+
+    // WebSocketの各イベントに対する処理を定義
+    socketInitializer(socket);
+
+    // グローバル状態にソケットを保存
+    setSocket(socket);
+  };
+
+  return { socketConnect };
+};
