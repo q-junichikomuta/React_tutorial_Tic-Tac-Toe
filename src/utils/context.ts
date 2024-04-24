f07925999dbe8e9583564a1169e3dfd84d977146
junichi komuta <komuta@q-co.jp>
2024-04-24 09:55:46 +0900

    不要なファイルを削除

--- src/utils/context.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-
-// 全コンポーネントでダークモードおよびレスポンシブを管理したいので、useContextを使用
-export const DarkModeContext = createContext(false);
-export const ResponsiveContext = createContext(false);
