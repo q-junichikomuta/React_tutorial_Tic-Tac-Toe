1dba8179fd95c306c8c461f0319dbdb5a47a1dcd
junichi komuta <komuta@q-co.jp>
2024-04-24 13:03:59 +0900

    WebSocket関連のモジュールをコメントアウト

--- src/components/matchGame.tsx
+++ src/components/matchGame.tsx
@@ -1,90 +1,90 @@
-'use client';
+// 'use client';
 
-import React, { useState } from 'react';
-import Message from '@/app/models/message';
-import { messageBoardAtom, socketAtom } from '@/app/globalStates/atoms';
-import { Button, Stack } from '@mui/material';
-import { Square } from './square';
-import { BoardGrid } from '@/utils/styleComponents';
-import { useAtom } from 'jotai';
-import { useSocket } from '@/hooks/useSocket';
+// import React, { useState } from 'react';
+// import Message from '@/app/models/message';
+// import { messageBoardAtom, socketAtom } from '@/app/globalStates/atoms';
+// import { Button, Stack } from '@mui/material';
+// import { Square } from './square';
+// import { BoardGrid } from '@/utils/styleComponents';
+// import { useAtom } from 'jotai';
+// import { useSocket } from '@/hooks/useSocket';
 
-// メッセージの入力と一覧を行うコンポーネント
-export function MatchGame() {
-  const oneSideNum = 3; // n目並べのn
+// // メッセージの入力と一覧を行うコンポーネント
+// export function MatchGame() {
+//   const oneSideNum = 3; // n目並べのn
 
-  const [isMatch, setIsMatch] = useState(false);
-  // 各グローバル状態のAtomを用意
-  const [messageBoard] = useAtom(messageBoardAtom);
-  const [socket] = useAtom(socketAtom);
+//   const [isMatch, setIsMatch] = useState(false);
+//   // 各グローバル状態のAtomを用意
+//   const [messageBoard] = useAtom(messageBoardAtom);
+//   const [socket] = useAtom(socketAtom);
 
-  const { socketConnect } = useSocket();
+//   const { socketConnect } = useSocket();
 
-  const matchStart = () => {
-    socketConnect();
-    setIsMatch(true);
-  };
+//   const matchStart = () => {
+//     socketConnect();
+//     setIsMatch(true);
+//   };
 
-  const currentMove = messageBoard.length - 1;
-  const latestSquares = messageBoard[currentMove].squareValue as Value[];
-  const nextPlayer = currentMove % 2 === 0;
-  const playerName = nextPlayer ? 'X' : 'O';
+//   const currentMove = messageBoard.length - 1;
+//   const latestSquares = messageBoard[currentMove].squareValue as Value[];
+//   const nextPlayer = currentMove % 2 === 0;
+//   const playerName = nextPlayer ? 'X' : 'O';
 
-  // squareを選択した時の処理
-  const squareClick = (i: number) => {
-    if (!isMatch) {
-      return;
-    }
+//   // squareを選択した時の処理
+//   const squareClick = (i: number) => {
+//     if (!isMatch) {
+//       return;
+//     }
 
-    latestSquares[i] = playerName; // 選択したsquareのvalueを更新
+//     latestSquares[i] = playerName; // 選択したsquareのvalueを更新
 
-    // 送信するメッセージを作成
-    const sendMessage: Message = {
-      id: crypto.randomUUID(), // UUIDを生成して各メッセージに固有のIDを付与
-      squareValue: latestSquares as string[],
-    };
+//     // 送信するメッセージを作成
+//     const sendMessage: Message = {
+//       id: crypto.randomUUID(), // UUIDを生成して各メッセージに固有のIDを付与
+//       squareValue: latestSquares as string[],
+//     };
 
-    // サーバーにメッセージを送信
-    socket.emit('message', sendMessage);
-  };
+//     // サーバーにメッセージを送信
+//     socket.emit('message', sendMessage);
+//   };
 
-  const restart = () => {
-    const sendMessage: Message = {
-      id: crypto.randomUUID(), // UUIDを生成して各メッセージに固有のIDを付与
-      squareValue: [...Array(oneSideNum * oneSideNum).fill(null)],
-    };
+//   const restart = () => {
+//     const sendMessage: Message = {
+//       id: crypto.randomUUID(), // UUIDを生成して各メッセージに固有のIDを付与
+//       squareValue: [...Array(oneSideNum * oneSideNum).fill(null)],
+//     };
 
-    // サーバーにメッセージを送信
-    socket.emit('message', sendMessage);
-  };
+//     // サーバーにメッセージを送信
+//     socket.emit('message', sendMessage);
+//   };
 
-  // valueを反映させたsquareを生成
-  const squareAll = [...Array(oneSideNum * oneSideNum)].map((_, i) => (
-    <Square
-      key={`Square-${i + 1}`} // 今回はindexでKeyを付けたが、ユニークIDなどがあればそれをつける
-      value={
-        messageBoard.length > 0 ? (messageBoard[messageBoard.length - 1].squareValue[i] as unknown as Value) : null
-      }
-      onClick={() => squareClick(i)}
-      winLine={false}
-    />
-  ));
+//   // valueを反映させたsquareを生成
+//   const squareAll = [...Array(oneSideNum * oneSideNum)].map((_, i) => (
+//     <Square
+//       key={`Square-${i + 1}`} // 今回はindexでKeyを付けたが、ユニークIDなどがあればそれをつける
+//       value={
+//         messageBoard.length > 0 ? (messageBoard[messageBoard.length - 1].squareValue[i] as unknown as Value) : null
+//       }
+//       onClick={() => squareClick(i)}
+//       winLine={false}
+//     />
+//   ));
 
-  // 生成したsquareをGridで正方形に整形
-  const PlayBoard = BoardGrid(oneSideNum);
+//   // 生成したsquareをGridで正方形に整形
+//   const PlayBoard = BoardGrid(oneSideNum);
 
-  return (
-    <>
-      <Stack spacing={2} direction={'column'} alignItems="center" justifyContent="center">
-        <div>{!isMatch ? `対戦開始ボタンを押してください` : `現在のプレイヤー：${playerName}`}</div>
-        <PlayBoard>{squareAll}</PlayBoard>
-        <Button onClick={matchStart} disabled={isMatch}>
-          {isMatch ? '対戦中' : '対戦開始'}
-        </Button>
-        <Button onClick={restart} color="error">
-          もう一度対戦する
-        </Button>
-      </Stack>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Stack spacing={2} direction={'column'} alignItems="center" justifyContent="center">
+//         <div>{!isMatch ? `対戦開始ボタンを押してください` : `現在のプレイヤー：${playerName}`}</div>
+//         <PlayBoard>{squareAll}</PlayBoard>
+//         <Button onClick={matchStart} disabled={isMatch}>
+//           {isMatch ? '対戦中' : '対戦開始'}
+//         </Button>
+//         <Button onClick={restart} color="error">
+//           もう一度対戦する
+//         </Button>
+//       </Stack>
+//     </>
+//   );
+// }
--- src/components/supabase.tsx
+++ src/components/supabase.tsx
@@ -1,65 +1,65 @@
-import { Button, Stack } from '@mui/material';
-import { createClient } from '@supabase/supabase-js';
-import { useEffect, useState } from 'react';
+// import { Button, Stack } from '@mui/material';
+// import { createClient } from '@supabase/supabase-js';
+// import { useEffect, useState } from 'react';
 
-type Sapmle = {
-  id: number;
-  name: string;
-};
+// type Sapmle = {
+//   id: number;
+//   name: string;
+// };
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
+// const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!);
 
-const db = 'sample';
+// const db = 'sample';
 
-export default function SupabaseComponent({ history }: { history: HistoryType[] }) {
-  const [foo, setFoo] = useState<Sapmle[]>([]);
-  // undefindが許容されているので、末尾に!を付けて許容しないようにする
+// export default function SupabaseComponent({ history }: { history: HistoryType[] }) {
+//   const [foo, setFoo] = useState<Sapmle[]>([]);
+//   // undefindが許容されているので、末尾に!を付けて許容しないようにする
 
-  // const output = () => {
-  //   const array = foo[3].name as unknown as string[];
-  //   console.log(array);
-  // };
+//   // const output = () => {
+//   //   const array = foo[3].name as unknown as string[];
+//   //   console.log(array);
+//   // };
 
-  const getSapmle = async () => {
-    const { data: sample } = await supabase.from(db).select();
-    setFoo(sample as Sapmle[]);
-  };
+//   const getSapmle = async () => {
+//     const { data: sample } = await supabase.from(db).select();
+//     setFoo(sample as Sapmle[]);
+//   };
 
-  const insertSapmle = async () => {
-    const { error } = await supabase.from(db).insert({ id: 5, name: 'Denmark' });
-  };
+//   const insertSapmle = async () => {
+//     const { error } = await supabase.from(db).insert({ id: 5, name: 'Denmark' });
+//   };
 
-  const updateSapmle = async () => {
-    const { error } = await supabase.from(db).update({ name: '西村さん' }).eq('id', 6);
-  };
+//   const updateSapmle = async () => {
+//     const { error } = await supabase.from(db).update({ name: '西村さん' }).eq('id', 6);
+//   };
 
-  const upsertSapmle = async () => {
-    const { error } = await supabase.from(db).upsert({ id: 2, name: 'アップさーと' }).select();
-  };
+//   const upsertSapmle = async () => {
+//     const { error } = await supabase.from(db).upsert({ id: 2, name: 'アップさーと' }).select();
+//   };
 
-  const deleteSapmle = async () => {
-    const { error } = await supabase.from(db).delete().eq('id', 3);
-  };
-  useEffect(() => {
-    getSapmle();
-  }, []);
+//   const deleteSapmle = async () => {
+//     const { error } = await supabase.from(db).delete().eq('id', 3);
+//   };
+//   useEffect(() => {
+//     getSapmle();
+//   }, []);
 
-  return (
-    <>
-      <Stack>
-        <Button onClick={insertSapmle}>インサート</Button>
-        <Button onClick={updateSapmle}>アップデート</Button>
-        <Button onClick={upsertSapmle}>アップサート</Button>
-        <Button onClick={deleteSapmle}>デリート</Button>
-        {/* <Button onClick={output}>アウトプット</Button> */}
-      </Stack>
-      <ul>
-        {foo.map((country) => (
-          <li key={country.name}>
-            {country.id}:{country.name}
-          </li>
-        ))}
-      </ul>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <Stack>
+//         <Button onClick={insertSapmle}>インサート</Button>
+//         <Button onClick={updateSapmle}>アップデート</Button>
+//         <Button onClick={upsertSapmle}>アップサート</Button>
+//         <Button onClick={deleteSapmle}>デリート</Button>
+//         {/* <Button onClick={output}>アウトプット</Button> */}
+//       </Stack>
+//       <ul>
+//         {foo.map((country) => (
+//           <li key={country.name}>
+//             {country.id}:{country.name}
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
